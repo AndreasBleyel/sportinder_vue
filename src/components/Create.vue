433da46f63cfaa33a17558f5185341196f733dcd
mlauf <stud.michael.lauf@fh-kufstein.ac.at>
2017-06-27 12:32:36 +0200

    verbindung zu loopback DB funktioniert. aktivitat wird in db gespeichert

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -9,13 +9,13 @@
     <form id="form">
       <div class="input-group">
         <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-        <input id="name" type="text" class="form-control" name="name" placeholder="Name der Aktivität">
+        <input id="name" type="text" class="form-control" name="name" placeholder="Name der Aktivität" v-model="activity.name">
       </div>
       <p></p>
 
       <div class="form-group">
         <label for="description">Beschreibung der Aktivität</label>
-        <textarea class="form-control" rows="5" id="description"></textarea>
+        <textarea class="form-control" rows="5" id="description" v-model="activity.description"></textarea>
       </div>
       <p></p>
 
@@ -27,13 +27,13 @@
 
       <div class="input-group">
         <span class="input-group-addon"><i class="glyphicon glyphicon-map-marker"></i></span>
-        <input id="street" type="text" class="form-control" name="straße" placeholder="Straße">
+        <input id="street" type="text" class="form-control" name="straße" placeholder="Straße" v-model="activity.startingadr">
       </div>
       <p></p>
 
       <div class="input-group">
         <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-        <input id="creator" type="text" class="form-control" name="ersteller" placeholder="Dein Name">
+        <input id="creator" type="text" class="form-control" name="ersteller" placeholder="Dein Name" v-model="activity.creator">
       </div>
       <p></p>
 
@@ -50,19 +50,19 @@
 
       <div class="input-group" data-time="now">
         <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-        <input id="time" class="form-control" name="zeit" placeholder="Startzeit">
+        <input id="time" class="form-control" name="zeit" placeholder="Startzeit" v-model="activity.startingtime">
       </div>
       <p></p>
 
       <div class="input-group">
         <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-        <input id="duration" class="form-control" name="dauer" placeholder="Dauer">
+        <input id="duration" class="form-control" name="dauer" placeholder="Dauer" v-model="activity.duration">
       </div>
       <p></p>
 
       <div class="input-group">
         <span class="input-group-addon"><i class="glyphicon glyphicon-tag"></i></span>
-        <select class="form-control" id="difficulty" placeholder="Schwierigkeitsstufe">
+        <select class="form-control" id="difficulty" placeholder="Schwierigkeitsstufe" v-model="activity.difficulty">
           <option selected hidden>Schwierigkeitsstufe</option>
           <option>leicht</option>
           <option>leicht - mittel</option>
@@ -77,7 +77,7 @@
 
     </form>
 
-    <button type="submit" class="btn btn-success"><i class="glyphicon glyphicon-ok"></i> Bestätigen</button>
+    <button v-on:click="submit" type="submit" class="btn btn-success"><i class="glyphicon glyphicon-ok"></i> Bestätigen</button>
     <p></p>
                     </div>
     </div>
@@ -89,9 +89,23 @@
     name: 'Create',
     data () {
       return {
-        msg: 'Create Page'
+        msg: 'Create Page',
+
+        activity: {name: '', description: '', creator: '', startingtime: '', difficulty: '', duration: '', startingadr: ''}
+
+
       }
-    }
+    },
+    methods: {
+            submit: function () {
+             this.$http.post('http://localhost:3000/api/activities', {name: this.activity.name, description: this.activity.description, creator: this.activity.creator, startingtime: this.activity.startingtime, difficulty: this.activity.difficulty, duration: this.activity.duration, startingadr: this.activity.startingadr}, function(response) {
+                 alert(response);
+
+             });
+             console.log(this.activity);
+            }
+
+          }
   }
 </script>
 
