8d1e9b27b984be8c82acd03b7dc5824a0c729b32
sandymanine <stud.sandra.rauscher@fh-kufstein.ac.at>
2017-06-27 11:10:52 +0200

    Formular

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,7 +1,80 @@
 <template>
   <div class="row">
     <div class="col-md-6 offset-md-3">
-      <h1>{{ msg }}</h1>
+  <div class="Create">
+    <h1>{{ msg }}</h1>
+
+
+
+    <form id="form">
+      <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+        <input id="name" type="text" class="form-control" name="name" placeholder="Name der Aktivität">
+      </div>
+      <p></p>
+
+      <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-text-size"></i></span>
+        <input id="description" type="text" class="form-control" name="beschreibung" placeholder="Kurzbeschreibung">
+      </div>
+      <p></p>
+
+      <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span>
+        <input id="city" type="city" class="form-control" name="stadt" placeholder="Stadt">
+      </div>
+      <p></p>
+
+      <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-map-marker"></i></span>
+        <input id="street" type="text" class="form-control" name="straße" placeholder="Straße">
+      </div>
+      <p></p>
+
+      <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+        <input id="creator" type="text" class="form-control" name="ersteller" placeholder="Dein Name">
+      </div>
+      <p></p>
+
+      <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+        <input id="date" type="text" class="form-control" name="datum" placeholder="Datum">
+      </div>
+      <p></p>
+
+      <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+        <input id="time" type="text" class="form-control" name="zeit" placeholder="Startzeit">
+      </div>
+      <p></p>
+
+      <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+        <input id="duration" type="text" class="form-control" name="dauer" placeholder="Dauer">
+      </div>
+      <p></p>
+
+      <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-tag"></i></span>
+        <select class="form-control" id="difficulty" placeholder="Schwierigkeitsstufe">
+          <option selected hidden>Schwierigkeitsstufe</option>
+          <option>leicht</option>
+          <option>leicht - mittel</option>
+          <option>mittel</option>
+          <option>mittel - schwer</option>
+          <option>schwer</option>
+        </select>
+      </div>
+      <p></p>
+
+      <p></p>
+
+    </form>
+
+    <button type="submit" class="btn btn-success"><i class="glyphicon glyphicon-ok"></i> Bestätigen</button>
+    <p></p>
+                    </div>
     </div>
   </div>
 </template>
