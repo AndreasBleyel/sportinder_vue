449ccbb6b5ec1cd2752860c1b5ed9f413cc19835
Benjamin <stud.benjamin.larcher@fh-kufstein.ac.at>
2017-07-04 13:27:11 +0200

    Navbar
    Änderung der navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="Navbar">
-
+ <b-link class="navbar-brand" to="/">
+                                                          <img src="../assets/sportinderLogo.png">
+                                                        </b-link>
     <b-navbar toggleable type="inverse" variant="standard">
 
       <b-nav-toggle target="nav_collapse"></b-nav-toggle>
 
-      <b-link class="navbar-brand" to="/">
-        <img src="../assets/sportinderLogo.png">
-      </b-link>
+
 
       <b-collapse is-nav id="nav_collapse">
 
         <b-nav is-nav-bar>
+        <b-nav-item>
+                    <router-link to="/">Startseite</router-link>
+                  </b-nav-item>
           <b-nav-item>
             <router-link to="List">Alle Aktivitäten</router-link>
           </b-nav-item>
@@ -36,5 +39,28 @@
 </script>
 
 <style>
+#nav_collapse {
+    background-color: #42b983;
+    overflow: hidden;
+}
 
+#nav_collapse a {
+    float: left;
+    display: block;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+}
+
+#nav_collapse a:hover {
+    background-color: #ddd;
+    color: black;
+}
+
+#nav_collapse a.active {
+    background-color: #4CAF50;
+    color: white;
+}
 </style>
